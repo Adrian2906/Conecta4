1fbdd8b48b4a6cc6707ded32b0776ca7eb42068e
Adrián <57500515+Adrian2906@users.noreply.github.com>
2022-11-01 22:55:44 +0100

    added logic to put pieces alternating colors

--- app/src/main/java/com/adrian/conecta4/MainActivity.kt
+++ app/src/main/java/com/adrian/conecta4/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.adrian.conecta4
 
+import android.annotation.SuppressLint
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.core.view.children
 
 class MainActivity : AppCompatActivity() {
+    private val MAX_ROW: Int = 5
+    private val MIN_ROW: Int = 0
+
     private lateinit var boardLinearLayout: LinearLayout
 
     private lateinit var turnTextView: TextView
@@ -16,6 +23,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var restartScoreboardButton: Button
     private lateinit var restartMatchButton: Button
 
+    //TODO: make it immutable
+    private lateinit var EMPTY_CELL: Drawable // in upper because it must be immutable
     //TODO: first match starts the RED piece
     // but second match starts the YELLOW piece
     // third RED and so on
@@ -25,6 +34,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        @SuppressLint("UseCompatLoadingForDrawables")
+        EMPTY_CELL = resources.getDrawable(R.drawable.empty_cell, theme)
         setTextViews()
         setBoard()
         setButtons()
@@ -38,11 +49,10 @@ class MainActivity : AppCompatActivity() {
 
     private fun setBoard() {
         boardLinearLayout = findViewById(R.id.boardLinearLayout)
-        //TODO: Columns (board's children) should have onClickListener not the board
-        boardLinearLayout.setOnClickListener {
-            when(currentPiece) {
-                Piece.RED -> changeToYellowPiece()
-                Piece.YELLOW -> changeToRedPiece()
+
+        for (column in boardLinearLayout.children) {
+            column.setOnClickListener { it as LinearLayout
+                play(it)
             }
         }
     }
@@ -64,23 +74,23 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun changeToRedPiece() {
-        sumScore(yellowScoreboardTextView)
-        setTurnTextViewBackgroundColor(R.color.red)
-        setCurrentPiece(Piece.RED)
+    private fun play(column: LinearLayout) {
+        val drawable = getDrawableFromPiece(currentPiece)
+        val played = putPieceOnBoard(column, drawable)
+
+        if (played) {
+            val nextPiece = currentPiece.otherPiece()
+            setTurnTextViewBackgroundColor(nextPiece.resourceColor)
+            setCurrentPiece(nextPiece)
+        }
     }
 
-    private fun changeToYellowPiece() {
-        sumScore(redScoreboardTextView)
-        setTurnTextViewBackgroundColor(R.color.yellow)
-        setCurrentPiece(Piece.YELLOW)
-    }
-
-    private fun sumScore(scoreboard: TextView) {
-        var score = scoreboard.text.toString().toInt()
-        score++
-        scoreboard.text = score.toString()
-    }
+    //TODO: use when game is ready
+//    private fun sumScore(scoreboard: TextView) {
+//        var score = scoreboard.text.toString().toInt()
+//        score++
+//        scoreboard.text = score.toString()
+//    }
 
     private fun setTurnTextViewBackgroundColor(resourceId: Int) {
         val color =  resources.getColor(resourceId)
@@ -90,6 +100,41 @@ class MainActivity : AppCompatActivity() {
     private fun setCurrentPiece(piece: Piece) {
         currentPiece = piece
     }
+
+    private fun putPieceOnBoard(column: LinearLayout, drawable: Drawable, index: Int = MAX_ROW): Boolean {
+        if (index < MIN_ROW) return false
+
+        val cell = column.getChildAt(index) as ImageView
+        if (cell.drawable.constantState != EMPTY_CELL.constantState) return putPieceOnBoard(column, drawable, index - 1)
+        cell.setImageDrawable(drawable)
+        return true
+    }
+
+    @SuppressLint("UseCompatLoadingForDrawables")
+    private fun getDrawableFromPiece(piece: Piece): Drawable {
+        return when(piece) {
+            Piece.RED -> resources.getDrawable(R.drawable.red_cell, theme)
+            Piece.YELLOW -> resources.getDrawable(R.drawable.yellow_cell, theme)
+        }
+    }
 }
 
-enum class Piece { RED, YELLOW }
+//TODO: extract to file
+enum class Piece {
+    RED {
+        override val resourceColor: Int
+            get() = R.color.red
+    },
+    YELLOW {
+        override val resourceColor: Int
+            get() = R.color.yellow
+    };
+
+    abstract val resourceColor: Int
+    fun otherPiece(): Piece {
+        return when (this) {
+            RED -> YELLOW
+            YELLOW -> RED
+        }
+    }
+}
