5a124931c77dd076753e3ba683cdd7b9992cfa60
Adrián <57500515+Adrian2906@users.noreply.github.com>
2022-11-01 14:18:05 +0100

    added scoreboard logic

--- app/src/main/java/com/adrian/conecta4/MainActivity.kt
+++ app/src/main/java/com/adrian/conecta4/MainActivity.kt
@@ -2,10 +2,69 @@ package com.adrian.conecta4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var boardLinearLayout: LinearLayout
+
+    private lateinit var turnTextView: TextView
+    private lateinit var redScoreboardTextView: TextView
+    private lateinit var yellowScoreboardTextView: TextView
+
+    private var currentPiece: Piece = Piece.RED
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setTextViews()
+        setBoard()
+    }
+
+    private fun setTextViews() {
+        redScoreboardTextView = findViewById(R.id.redScoreboardTextView)
+        yellowScoreboardTextView = findViewById(R.id.yellowScoreboardTextView)
+        turnTextView = findViewById(R.id.turnTextView)
+    }
+
+    private fun setBoard() {
+        boardLinearLayout = findViewById(R.id.boardLinearLayout)
+        //added temporally logic to board. Columns (board's children) should have the logic
+        boardLinearLayout.setOnClickListener {
+            when(currentPiece) {
+                Piece.RED -> changeToYellowPiece()
+                Piece.YELLOW -> changeToRedPiece()
+            }
+        }
+    }
+
+    private fun changeToRedPiece() {
+        sumScore(yellowScoreboardTextView)
+        setTurnTextViewBackgroundColor(R.color.red)
+        setCurrentPiece(Piece.RED)
+    }
+
+    private fun changeToYellowPiece() {
+        sumScore(redScoreboardTextView)
+        setTurnTextViewBackgroundColor(R.color.yellow)
+        setCurrentPiece(Piece.YELLOW)
+    }
+
+    private fun sumScore(scoreboard: TextView) {
+        var score = scoreboard.text.toString().toInt()
+        score++
+        scoreboard.text = score.toString()
+    }
+
+    private fun setTurnTextViewBackgroundColor(resourceId: Int) {
+        val color =  resources.getColor(resourceId)
+        turnTextView.setBackgroundColor(color)
+    }
+
+    private fun setCurrentPiece(piece: Piece) {
+        currentPiece = piece
     }
 }
+
+enum class Piece { RED, YELLOW }
