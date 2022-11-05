a6047987e173e5180d97c4615e7b62061897f111
Adrián <57500515+Adrian2906@users.noreply.github.com>
2022-11-05 22:58:14 +0100

    added win logic

--- app/src/main/java/com/adrian/conecta4/MainActivity.kt
+++ app/src/main/java/com/adrian/conecta4/MainActivity.kt
@@ -8,11 +8,16 @@ import android.widget.Button
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
+import android.widget.Toast
 import androidx.core.view.children
 
 class MainActivity : AppCompatActivity() {
-    private val MAX_ROW: Int = 5
-    private val MIN_ROW: Int = 0
+    private val MAX_INDEX_ROW: Int = 5
+    private val MIN_INDEX_ROW: Int = 0
+    private val MIN_INDEX_COLUMN: Int = 0
+    private val MAX_INDEX_COLUMN: Int = 6
+    private val CAN_NOT_MAKE_MOVE: Int = -1
+    private val ALIGNED_PIECES_TO_WIN: Int = 4
 
     private lateinit var boardLinearLayout: LinearLayout
 
@@ -70,28 +75,56 @@ class MainActivity : AppCompatActivity() {
         restartMatchButton.setOnClickListener {
             //TODO: restart match so current piece will be the start piece
             setCurrentPiece(Piece.RED)
-            setTurnTextViewBackgroundColor(R.color.red)
+            setTurnTextViewBackgroundColor(Piece.RED.resourceColorId)
+            clearBoard()
         }
     }
 
     @SuppressLint("UseCompatLoadingForDrawables")
     private fun play(column: LinearLayout) {
+        val columnIndex = boardLinearLayout.indexOfChild(column)
         val drawable = resources.getDrawable(currentPiece.resourceDrawableId, theme)
-        val played = putPieceOnBoard(column, drawable)
+        val rowIndexPlayed = putPieceOnBoard(column, drawable)
 
-        if (played) {
-            val nextPiece = currentPiece.otherPiece
-            setTurnTextViewBackgroundColor(nextPiece.resourceColorId)
-            setCurrentPiece(nextPiece)
+        if (CAN_NOT_MAKE_MOVE == rowIndexPlayed) return
+
+        val isWinner = isWinner(columnIndex, rowIndexPlayed, drawable)
+
+        if (isWinner) {
+            Toast.makeText(this@MainActivity, "FOO", Toast.LENGTH_LONG).show()
+            sumScore()
+            clearBoard()
+            return
+        }
+
+        val nextPiece = currentPiece.otherPiece
+        setTurnTextViewBackgroundColor(nextPiece.resourceColorId)
+        setCurrentPiece(nextPiece)
+    }
+
+    private fun clearBoard() {
+        val columns = boardLinearLayout.children
+        columns.forEach { column ->
+            column as LinearLayout
+            column.children.forEach { it as ImageView
+                it.setImageDrawable(EMPTY_CELL)
+            }
         }
     }
 
-    //TODO: use when game is ready
-//    private fun sumScore(scoreboard: TextView) {
-//        var score = scoreboard.text.toString().toInt()
-//        score++
-//        scoreboard.text = score.toString()
-//    }
+    private fun sumScore() {
+        val scoreboard = getScoreboardTextViewByCurrentPiece()
+        var score = scoreboard.text.toString().toInt()
+        score++
+        scoreboard.text = score.toString()
+    }
+
+    private fun getScoreboardTextViewByCurrentPiece(): TextView {
+        return when(currentPiece) {
+            Piece.RED -> redScoreboardTextView
+            Piece.YELLOW -> yellowScoreboardTextView
+        }
+    }
 
     private fun setTurnTextViewBackgroundColor(resourceId: Int) {
         val color =  resources.getColor(resourceId)
@@ -102,12 +135,175 @@ class MainActivity : AppCompatActivity() {
         currentPiece = piece
     }
 
-    private fun putPieceOnBoard(column: LinearLayout, drawable: Drawable, index: Int = MAX_ROW): Boolean {
-        if (index < MIN_ROW) return false
+    private fun putPieceOnBoard(column: LinearLayout, drawable: Drawable, index: Int = MAX_INDEX_ROW): Int {
+        if (index < MIN_INDEX_ROW)
+            return CAN_NOT_MAKE_MOVE
 
         val cell = column.getChildAt(index) as ImageView
-        if (cell.drawable.constantState != EMPTY_CELL.constantState) return putPieceOnBoard(column, drawable, index - 1)
+
+        if (cell.drawable.constantState != EMPTY_CELL.constantState)
+            return putPieceOnBoard(column, drawable, index - 1)
+
         cell.setImageDrawable(drawable)
-        return true
+        return index
+    }
+
+    private fun isWinner(columnIndex: Int, rowIndex: Int, drawable: Drawable): Boolean {
+        //TODO: REFACTOR
+        var alignedPiecesCount = 1
+
+        //CHECK HORIZONTAL PIECES
+        //LEFT
+        var brakeChain = false
+        var index = columnIndex
+
+        while (index > MIN_INDEX_COLUMN && !brakeChain) {
+            index--
+            val column = boardLinearLayout.getChildAt(index) as LinearLayout
+            val cell = column.getChildAt(rowIndex) as ImageView
+
+            if (cell.drawable.constantState == drawable.constantState)
+                alignedPiecesCount += 1
+            else
+                brakeChain = true
+        }
+        //LEFT
+
+        //RIGHT
+        brakeChain = false
+        index = columnIndex
+
+        while (index < MAX_INDEX_COLUMN && !brakeChain) {
+            index++
+            val column = boardLinearLayout.getChildAt(index) as LinearLayout
+            val cell = column.getChildAt(rowIndex) as ImageView
+
+            if (cell.drawable.constantState == drawable.constantState)
+                alignedPiecesCount += 1
+            else
+                brakeChain = true
+        }
+        //RIGHT
+        //CHECK HORIZONTAL PIECES
+
+        if (alignedPiecesCount >= ALIGNED_PIECES_TO_WIN) return true
+        alignedPiecesCount = 1
+
+        //CHECK VERTICAL PIECES
+        val playedColumn = boardLinearLayout.getChildAt(columnIndex) as LinearLayout
+
+        //DOWN
+        brakeChain = false
+        index = rowIndex
+
+        while (index < MAX_INDEX_ROW && !brakeChain) {
+            index++
+            val cell = playedColumn.getChildAt(index) as ImageView
+
+            if (cell.drawable.constantState == drawable.constantState)
+                alignedPiecesCount += 1
+            else
+                brakeChain = true
+        }
+        //DOWN
+
+        //UP
+        brakeChain = false
+        index = rowIndex
+
+        while (index > MIN_INDEX_ROW && !brakeChain) {
+            index--
+            val cell = playedColumn.getChildAt(index) as ImageView
+
+            if (cell.drawable.constantState == drawable.constantState)
+                alignedPiecesCount += 1
+            else
+                brakeChain = true
+        }
+        //UP
+        //CHECK VERTICAL PIECES
+
+        if (alignedPiecesCount >= ALIGNED_PIECES_TO_WIN) return true
+        alignedPiecesCount = 1
+
+        //CHECK DIAGONAL PIECES
+        brakeChain = false
+        var horizontalIndex = columnIndex
+        var verticalIndex = rowIndex
+
+        //DOWN AND LEFT
+        while (horizontalIndex > MIN_INDEX_COLUMN && verticalIndex < MAX_INDEX_ROW && !brakeChain) {
+            horizontalIndex--
+            verticalIndex++
+
+            val column = boardLinearLayout.getChildAt(horizontalIndex) as LinearLayout
+            val cell = column.getChildAt(verticalIndex) as ImageView
+
+            if (cell.drawable.constantState == drawable.constantState)
+                alignedPiecesCount += 1
+            else
+                brakeChain = true
+        }
+        //DOWN AND LEFT
+
+        //DOWN AND RIGHT
+        brakeChain = false
+        horizontalIndex = columnIndex
+        verticalIndex = rowIndex
+
+        while (horizontalIndex < MAX_INDEX_COLUMN && verticalIndex < MAX_INDEX_ROW && !brakeChain) {
+            horizontalIndex++
+            verticalIndex++
+
+            val column = boardLinearLayout.getChildAt(horizontalIndex) as LinearLayout
+            val cell = column.getChildAt(verticalIndex) as ImageView
+
+            if (cell.drawable.constantState == drawable.constantState)
+                alignedPiecesCount += 1
+            else
+                brakeChain = true
+        }
+        //DOWN AND RIGHT
+
+        //UP AND RIGHT
+        brakeChain = false
+        horizontalIndex = columnIndex
+        verticalIndex = rowIndex
+
+        while (horizontalIndex < MAX_INDEX_COLUMN && verticalIndex > MIN_INDEX_ROW && !brakeChain) {
+            horizontalIndex++
+            verticalIndex--
+
+            val column = boardLinearLayout.getChildAt(horizontalIndex) as LinearLayout
+            val cell = column.getChildAt(verticalIndex) as ImageView
+
+            if (cell.drawable.constantState == drawable.constantState)
+                alignedPiecesCount += 1
+            else
+                brakeChain = true
+        }
+        //UP AND RIGHT
+
+        //UP AND LEFT
+        brakeChain = false
+        horizontalIndex = columnIndex
+        verticalIndex = rowIndex
+
+        while (horizontalIndex > MIN_INDEX_COLUMN && verticalIndex > MIN_INDEX_ROW && !brakeChain) {
+            horizontalIndex--
+            verticalIndex--
+
+            val column = boardLinearLayout.getChildAt(horizontalIndex) as LinearLayout
+            val cell = column.getChildAt(verticalIndex) as ImageView
+
+            if (cell.drawable.constantState == drawable.constantState)
+                alignedPiecesCount += 1
+            else
+                brakeChain = true
+        }
+        //UP AND LEFT
+        //CHECK DIAGONAL PIECES
+
+        return alignedPiecesCount >= ALIGNED_PIECES_TO_WIN
     }
 }
