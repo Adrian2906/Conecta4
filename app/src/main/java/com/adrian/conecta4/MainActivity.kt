a792dfbb75e0450778863427c86e82998229401b
Adrián <57500515+Adrian2906@users.noreply.github.com>
2022-11-05 12:45:21 +0100

    refactor: Piece, extracted to own file

--- app/src/main/java/com/adrian/conecta4/MainActivity.kt
+++ app/src/main/java/com/adrian/conecta4/MainActivity.kt
@@ -111,31 +111,3 @@ class MainActivity : AppCompatActivity() {
         return true
     }
 }
-
-//TODO: extract to file
-enum class Piece {
-    RED {
-        override val resourceColorId: Int
-            get() = R.color.red
-
-        override val resourceDrawableId: Int
-            get() = R.drawable.red_cell
-
-        override val otherPiece: Piece
-            get() = YELLOW
-    },
-    YELLOW {
-        override val resourceColorId: Int
-            get() = R.color.yellow
-
-        override val resourceDrawableId: Int
-            get() = R.drawable.yellow_cell
-
-        override val otherPiece: Piece
-            get() = RED
-    };
-
-    abstract val resourceColorId: Int
-    abstract val resourceDrawableId: Int
-    abstract val otherPiece: Piece
-}
